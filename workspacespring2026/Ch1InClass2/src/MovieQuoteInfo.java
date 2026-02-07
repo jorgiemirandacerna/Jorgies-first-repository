beac9f8d557411fb27c7e4f2ca10192183bff757
jorgiemirandacerna <jorgie.mirandacerna@gmail.com>
2026-02-06 22:46:16 -0500

    Add files via upload

---
+++ workspacespring2026/Ch1InClass2/src/MovieQuoteInfo.java
@@ -0,0 +1,10 @@
+//Miranda Cerna 1/26 ch 1 in class 2
+public class MovieQuoteInfo {
+
+	public static void main(String[]args) {
+		System.out.println("I always come back");
+		System.out.println("Five Nights at Freddy's");
+		System.out.println("William Afton");
+		System.out.println("Year 2023");
+	}
+}
